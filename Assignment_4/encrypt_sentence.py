43081ed710d946dd4487325928007af06a991164
Vishesh Mishra <vishesh@Visheshs-MacBook-Air.local>
2022-06-17 14:54:44 +0530

    encrypt_sentence()

---
+++ Assignment_4/encrypt_sentence.py
@@ -0,0 +1,24 @@
+vowels=['a','e','i','o','u']
+
+def encrypt_sentence(sentence):
+    final=[]
+    list_sentence = sentence.split(" ")
+    for index,word in enumerate(list_sentence):
+        if (index+1)%2!=0:
+            final.append(word[::-1])
+        else:
+            v=[]
+            t=[]
+            for letter in word:
+                if letter not in vowels:
+                    t.append(letter)
+                else:
+                    v.append(letter)
+            t.extend(v)
+            final.append("".join(t))
+
+    return " ".join(final)
+
+sentence="The sun rises in the east"
+encrypted_sentence=encrypt_sentence(sentence)
+print(encrypted_sentence)
